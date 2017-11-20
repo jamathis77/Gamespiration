89aa50539abd90a81e2831957ef75ce659fba742
Jacob Mathis <jamathis77@gmail.com>
2017-11-20 09:25:12 -0500

    changed layout

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-//This creates the viewer. The urlid changes the model in the viewer
+
+
+//This creates the 3D model viewer. The urlid controls which model is in the viewer
 function viewer(urlid){
   var iframe = document.getElementById( 'api-frame' );
   var version = '1.0.0';
-  //urlid is the searchterm
+  //urlid is the searchterm that is passed in as the argument
   var urlid = urlid;
-  console.log(urlid)
   var client = new Sketchfab( version, iframe );
 
   client.init( urlid, {
@@ -24,7 +25,7 @@ function viewer(urlid){
   } );
 }
 
-//giantbomb.com api key e7729b1f9985d919624e31f7c1b2cabe1354aee7
+//ajax call to sketchfab for the 3d model.
 function getDataFromSketchfabApi(searchWord, callback){
   const settings = {
     url: 'https://api.sketchfab.com/v3/search',
@@ -39,15 +40,54 @@ function getDataFromSketchfabApi(searchWord, callback){
 
   $.ajax(settings)
 }
+//callback function for sketchfab ajax
+function callbackSketchfab(response){
+  let counter = 0;
+  $('.carousel-item').remove();
+  const results = response.results;
+  response.results.forEach(result => {
+    const uid = result.thumbnails.images[3]['uid'];
+    const url = result.thumbnails.images[3]['url'];
+    const models = result.thumbnails.images;
+    models.forEach(modelLink => {
 
-function renderSketchfabModels(imageUrl, imageUid, results){
-  const sketchfabModelElement = `
+      if(modelLink.url === url){
+        counter++;
+        renderSketchfabModels(url, uid, results, counter)
 
-      <a class="modelLink" value="${imageUid}"><img class="modelImage" value="${imageUid}" src="${imageUrl}" ></a>
+      }
+    })
 
+  });
+  //sends the first model to the viewer when a search is executed
+
+  viewer(results[0].uid)
+
+
+}
+
+function renderSketchfabModels(imageUrl, imageUid, results, counter){
+  const sketchfabModelElementActive = `
+  <div class="carousel-item active">
+    <a class="modelLink" value="${imageUid}"><img class="d-block img-fluid modelImage" value="${imageUid}" src="${imageUrl}" ></a>
+  </div>
   `;
-  $('.models').append(sketchfabModelElement);
+
+  const sketchfabModelElement = `
+  <div class="carousel-item">
+    <a class="modelLink" value="${imageUid}"><img class="d-block modelImage" value="${imageUid}" src="${imageUrl}" ></a>
+  </div>
+  `;
+  console.log(`counter is ${counter}`)
+  if(counter === 1){
+    $('.carousel-inner').append(sketchfabModelElementActive);
+  }else{
+    $('.carousel-inner').append(sketchfabModelElement);
+  }
+
+  // Adds ability to change model by clicking
   $('.modelLink').on('click', function(event){
+    console.log('clicked');
     let uid = event.target;
     let uidValue = $(uid).attr('value')
       results.forEach(result => {
@@ -57,22 +97,6 @@ function renderSketchfabModels(imageUrl, imageUid, results){
         }
       })
   })
-}
-
-function callbackSketchfab(response){
-  const results = response.results;
-  response.results.forEach(result => {
-    const uid = result.thumbnails.images[3]['uid'];
-    const url = result.thumbnails.images[3]['url'];
-    // const modelUid = result.uid;
-    const models = result.thumbnails.images;
-    models.forEach(modelLink => {
-      if(modelLink.url === url){
-        renderSketchfabModels(url, uid, results)
-      }
-    })
-  });
-  viewer(results[0].uid)
 }
 
 function getYouTubeApi(searchWord, callback){
@@ -100,13 +124,6 @@ function renderResult(title, thumbnail, link){
       <p class="youtubeTitle">${title}</p>
     </div>
   `;
-
-  // if(document.body.contains(hasSearchElement)){
-  //   hasSearchElement.remove();
-  // }
-  // if($('.videos').length){
-  //   hasSearchElement.remove()
-  // }
   $('.videos').append(searchElement);
 }
 
@@ -120,20 +137,65 @@ function callbackYouTubeSearchData(response){
   })
 }
 
+// function getSliderSettings(){
+//   return {
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     variableWidth: true
+//   }
+// }
+
+// $.ajax({
+//   type: 'get',
+//   url: '/public/index',
+//   dataType: 'script',
+//   data: data_send,
+//   success: function() {
+//     $('.models').slick('unslick'); /* ONLY remove the classes and handlers added on initialize */
+//     // $('.my-slide').remove(); /* Remove current slides elements, in case that you want to show new slides. */
+//     $('.models').slick(getSliderSettings()); /* Initialize the slick again */
+//   }
+// });
+
+// function addSlick(){
+//   console.log('slick has run')
+//
+//     $('.models').slick({
+//       dots: true,
+//       infinite: true,
+//       speed: 300,
+//       slidesToShow: 1,
+//       centerMode: true,
+//       variableWidth: true
+//
+//   });
+// }
+
 function watchSubmit(){
+
   $('.js-search-form').submit(event => {
+    let counter = 0;
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find('.js-query');
     const query = queryTarget.val();
     queryTarget.val("");
     const hasSearchElement = $('.search-result-block');
     const hasModelLink = $('.modelLink');
+    if(counter === 0){
+      $("section").removeClass("hidden")
+      $("iframe").removeClass("hidden")
+    }
     if($('.videos').length){
       hasSearchElement.remove();
     }
     if($('.models').length){
       hasModelLink.remove();
     }
+    counter++;
+    // $('.slider').slick('unslick');
     getDataFromSketchfabApi(query, callbackSketchfab);
     getYouTubeApi(query, callbackYouTubeSearchData);
 
